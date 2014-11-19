ccafdd45ef8d7cbc0445f3894a09f616cb65b495
Olivier Sallou <olivier.sallou@irisa.fr>
2014-11-19 17:43:29 +0100

    use factory for common functions

--- biomajwatcher/webapp/app/scripts/biomaj.js
+++ biomajwatcher/webapp/app/scripts/biomaj.js
@@ -270,47 +270,11 @@ angular.module('biomaj').controller('banksCtrl',
     });
 
 angular.module('biomaj').controller('searchCtrl',
-    function ($scope, $log, $location, Search) {
+    function ($scope, $log, $location, Search, ReorderSearchResults) {
       var query = $location.search().q
       $scope.query = query;
       Search.list({q: query}).$promise.then(function(res) {
-        var banks = []
-        for(var i=0;i<res.length;i++){
-          var id = res[i]['_source']['bank'];
-          var bank = {};
-          var new_bank = true;
-          for(var b=0;b<banks.length;b++){
-            if(banks[b]['name'] == id)
-              bank = banks[b];
-              new_bank = false;
-              break;
-          }
-          if(new_bank) {
-            bank['name'] = id;
-            bank['release'] = [];
-            banks.push(bank);
-          }
-          var new_release = true;
-          var release = res[i]['_source']['release'];
-          var rel = [];
-          for(var r=0;r<bank['release'];r++) {
-            if(bank['release'][r]['name'] == release) {
-              rel = bank['release'][r]['elts'];
-              new_release = false;
-              break;
-            }
-          }
-          if(new_release) {
-            rel = {'name': release, 'elts': []};
-            bank['release'].push(rel);
-          }
-          rel['elts'].push({'format': res[i]['_source']['format'],
-                                            'types': res[i]['_source']['types'],
-                                            'tags': res[i]['_source']['tags']
-
-            });
-
-        }
+        var banks = ReorderSearchResults.reorder(res);
         $scope.result = banks;
       });
     });
--- biomajwatcher/webapp/app/scripts/resources.js
+++ biomajwatcher/webapp/app/scripts/resources.js
@@ -3,15 +3,63 @@
 (function () {
   'use strict';
 
-    function Stat($resource) {
-        return $resource('/stat', {}, {
-            list: {
-              method: 'GET',
-              isArray: true,
-              cache: true
-            }
-        });
-    };
+  /**
+  * Reorder index search results, grouping bank and releases
+  */
+  function ReorderSearchResults(){
+    return {
+            reorder: function(res) {
+                  var banks = []
+                  for(var i=0;i<res.length;i++){
+                    var id = res[i]['_source']['bank'];
+                    var bank = {};
+                    var new_bank = true;
+                    for(var b=0;b<banks.length;b++){
+                      if(banks[b]['name'] == id)
+                        bank = banks[b];
+                        new_bank = false;
+                        break;
+                    }
+                    if(new_bank) {
+                      bank['name'] = id;
+                      bank['release'] = [];
+                      banks.push(bank);
+                    }
+                    var new_release = true;
+                    var release = res[i]['_source']['release'];
+                    var rel = [];
+                    for(var r=0;r<bank['release'];r++) {
+                      if(bank['release'][r]['name'] == release) {
+                        rel = bank['release'][r]['elts'];
+                        new_release = false;
+                        break;
+                      }
+                    }
+                    if(new_release) {
+                      rel = {'name': release, 'elts': []};
+                      bank['release'].push(rel);
+                    }
+                    rel['elts'].push({'format': res[i]['_source']['format'],
+                                                      'types': res[i]['_source']['types'],
+                                                      'tags': res[i]['_source']['tags']
+
+                      });
+
+                  }
+                  return banks;
+                }
+    }
+  };
+
+  function Stat($resource) {
+      return $resource('/stat', {}, {
+          list: {
+            method: 'GET',
+            isArray: true,
+            cache: true
+          }
+      });
+  };
 
     function Search($resource) {
         return $resource('/search', {}, {
@@ -78,6 +126,7 @@
 
 
   angular.module('biomaj.resources', ['ngResource'])
+    .factory('ReorderSearchResults', ReorderSearchResults)
     .factory('Search', Search)
     .factory('Stat', Stat)
     .factory('Bank', Bank)
